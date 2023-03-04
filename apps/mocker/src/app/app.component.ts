55ef1ec18c0fa9bf12a34a800b7533be93a7bf37
BaranovaAnastasia <aabaranova_3@edu.hse.ru>
2023-03-04 21:14:59 +0300

    chore: change detection

--- apps/mocker/src/app/app.component.ts
+++ apps/mocker/src/app/app.component.ts
@@ -1,10 +1,9 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
 	selector: 'mocker-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.less'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-	title = 'mocker';
-}
+export class AppComponent {}
--- apps/mocker/src/app/components/header/header.component.ts
+++ apps/mocker/src/app/components/header/header.component.ts
@@ -1,8 +1,9 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
 	selector: 'mocker-header',
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.less'],
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {}
